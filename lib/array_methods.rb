cf5885badcf9cc1fcbe170de65338762ef5ec375
Buford Eeds <bufordeeds8@gmail.com>
2019-09-10 18:39:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  puts array[value_to_find]
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
@@ -9,3 +9,7 @@ end
 def find_min_value(array)
   # Add your solution here
 end
+
+array = [1,2,3,4,5]
+value_to_find = 1
+
